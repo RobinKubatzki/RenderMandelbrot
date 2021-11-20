60826957ee413661c51905a2b25628b3e1783dc7
RobinKubatzki <rkubatzki95@gmx.de>
2021-11-20 01:35:49 +0100

    delete useless stuff

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
@@ -135,7 +132,6 @@ public class Controller : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0)){
